147d75e66a66b8afdd20037c95917b222905c7ef
Ahmed Abdo <devahmedabdo@gmail.com>
2023-03-31 16:56:47 +0200

    edit result box

--- src/app/views/home/components/landing/landing.component.ts
+++ src/app/views/home/components/landing/landing.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faChartPie,
+  faLocationArrow,
+  faMap,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import Swiper from 'swiper';
 
 @Component({
@@ -8,8 +15,15 @@ import Swiper from 'swiper';
 })
 export class LandingComponent implements OnInit {
   constructor() {}
+  location = faLocationArrow;
+  pieceIcon = faChartPie;
+  chartIcon = faMap;
+  search = faSearch;
+  user = faUser;
+  city: boolean = false;
+  chart: boolean = false;
+  piece: boolean = false;
+  startSearch: boolean = false;
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 }
